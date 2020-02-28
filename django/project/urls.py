b4d67d2707d87423f972c5b65c3b5bc0ff2decd6
Tatu Arvela <tatu.arvela@gmail.com>
2020-02-28 10:33:38 +0200

    Add Django implementation

---
+++ django/project/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import include
+from django.urls import path, re_path
+from django.contrib import admin
+from boards import views
+
+urlpatterns = [
+    path("", views.root, name="root"),
+    re_path(r"^boards/$", views.boards, name="boards"),
+    path("boards/<int:pk>/", views.board_topics, name="board_topics"),
+    path("boards/<int:pk>/new/", views.new_topic, name="new_topic"),
+    path("topics/<int:pk>/", views.topic_replies, name="topic_replies"),
+    path("admin/", admin.site.urls),
+]
+
+# Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path("accounts/", include("django.contrib.auth.urls")),
+]
