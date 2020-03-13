ce62762cbb8612ac76802bab651335dbcd9ed262
Tatu Arvela <tatu.arvela@gmail.com>
2020-03-13 16:44:13 +0200

    django: More tests for boards

--- django/forum/tests/test_boards.py
+++ django/forum/tests/test_boards.py
@@ -1,8 +1,14 @@
-from django.contrib.auth.models import User
-from django.urls import reverse, resolve
+from django.contrib.auth.models import User, Permission
+from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
-from ..views.boards import index as index_view
-from ..models import Board
+from django.urls import reverse, resolve
+from ..views.boards import (
+    index as index_view,
+    show as show_view,
+    new as new_view,
+    delete as delete_view,
+)
+from ..models import Board, Thread
 
 
 class RootRedirectTests(TestCase):
@@ -19,14 +25,14 @@ class RootRedirectTests(TestCase):
 
 class BoardsIndexTests(TestCase):
     def setUp(self):
-        test_user = User.objects.create_user(
+        self.user = User.objects.create_user(
             username="john", email="john@doe.com", password="123"
         )
         self.board = Board.objects.create(
             title="Django",
             description="Django board.",
-            created_by=test_user,
-            updated_by=test_user,
+            created_by=self.user,
+            updated_by=self.user,
         )
         url = reverse("boards")
         self.response = self.client.get(url)
@@ -42,23 +48,125 @@ class BoardsIndexTests(TestCase):
         show_url = reverse("boards_show", kwargs={"pk": self.board.pk})
         self.assertContains(self.response, 'href="{0}"'.format(show_url))
 
+
 class BoardsShowTests(TestCase):
     def setUp(self):
-        test = 1
+        self.user = User.objects.create_user(
+            username="john", email="john@doe.com", password="123"
+        )
+        self.board = Board.objects.create(
+            title="Django",
+            description="Django board.",
+            created_by=self.user,
+            updated_by=self.user,
+        )
+        self.thread = Thread.objects.create(
+            title="How to test Django apps?",
+            board=self.board,
+            created_by=self.user,
+            updated_by=self.user,
+        )
+        url = reverse("boards_show", kwargs={"pk": self.board.pk})
+        self.response = self.client.get(url)
 
-    def test_todo(self):
-        self.assertEquals(1, 1)
+    def test_show_view_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_non_existent_board_not_found(self):
+        url = reverse("boards_show", kwargs={"pk": 2})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_show_url_resolves_show_view(self):
+        view = resolve("/boards/{0}/".format(self.board.pk))
+        self.assertEquals(view.func, show_view)
+
+    def test_show_board_view_contains_link_to_show_thread_page(self):
+        show_thread_url = reverse("threads_show", kwargs={"pk": self.thread.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(show_thread_url))
+
 
 class BoardsNewTests(TestCase):
     def setUp(self):
-        test = 1
+        self.basic_user = User.objects.create_user(
+            username="john", email="john@doe.com", password="123"
+        )
+        self.admin_user = User.objects.create_user(
+            username="admin", email="admin@admin.com", password="admin"
+        )
 
-    def test_todo(self):
-        self.assertEquals(1, 1)
+        content_type = ContentType.objects.get_for_model(Board)
+        permission = Permission.objects.get(
+            content_type=content_type, codename="add_board"
+        )
+
+        self.admin_user.user_permissions.add(permission)
+
+        self.board = Board.objects.create(
+            title="Django",
+            description="Django board.",
+            created_by=self.basic_user,
+            updated_by=self.basic_user,
+        )
+
+    def test_new_view_status_code_as_not_permitted_user(self):
+        self.client.force_login(self.basic_user)
+        url = reverse("boards_new")
+        self.response = self.client.get(url)
+        self.assertEquals(self.response.status_code, 302)
+
+    def test_new_view_status_code_as_permitted_user(self):
+        self.client.force_login(self.admin_user)
+        url = reverse("boards_new")
+        self.response = self.client.get(url)
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_new_url_resolves_new_view(self):
+        view = resolve("/boards/new/")
+        self.assertEquals(view.func, new_view)
+
 
 class BoardsDeleteTests(TestCase):
     def setUp(self):
-        test = 1
+        self.basic_user = User.objects.create_user(
+            username="john", email="john@doe.com", password="123"
+        )
+        self.admin_user = User.objects.create_user(
+            username="admin", email="admin@admin.com", password="admin"
+        )
 
-    def test_todo(self):
-        self.assertEquals(1, 1)
+        content_type = ContentType.objects.get_for_model(Board)
+        permission = Permission.objects.get(
+            content_type=content_type, codename="delete_board"
+        )
+
+        self.admin_user.user_permissions.add(permission)
+
+        self.board = Board.objects.create(
+            title="Django",
+            description="Django board.",
+            created_by=self.basic_user,
+            updated_by=self.basic_user,
+        )
+
+    def test_new_view_status_code_as_not_permitted_user(self):
+        self.client.force_login(self.basic_user)
+        url = reverse("boards_delete", kwargs={"pk": self.board.pk})
+        self.response = self.client.post(url)
+        self.assertEquals(self.response.status_code, 302)
+
+    def test_new_view_status_code_as_permitted_user(self):
+        self.client.force_login(self.admin_user)
+        url = reverse("boards_delete", kwargs={"pk": self.board.pk})
+        self.response = self.client.post(url)
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_new_view_status_code_as_permitted_user_nonexistent_board(self):
+        self.client.force_login(self.admin_user)
+        url = reverse("boards_delete", kwargs={"pk": 2})
+        self.response = self.client.post(url)
+        self.assertEquals(self.response.status_code, 404)
+
+    def test_new_url_resolves_new_view(self):
+        view = resolve("/boards/delete/{0}/".format(self.board.pk))
+        self.assertEquals(view.func, delete_view)
--- django/project/urls.py
+++ django/project/urls.py
@@ -9,17 +9,17 @@ urlpatterns = [
     path("", boards.root_redirect, name="root"),
     re_path(r"^boards/$", boards.index, name="boards"),
     path("boards/<int:pk>/", boards.show, name="boards_show"),
-    re_path(r"^boards/new$", boards.new, name="boards_new"),
+    path("boards/new/", boards.new, name="boards_new"),
     path("boards/delete/<int:pk>/", boards.delete, name="boards_delete"),
     # Threads
     re_path(r"^threads/$", threads.index, name="threads"),
     path("threads/<int:pk>/", threads.show, name="threads_show"),
-    re_path(r"^threads/new$", threads.new, name="threads_new"),
+    re_path(r"^threads/new/$", threads.new, name="threads_new"),
     path("threads/delete/<int:pk>/", threads.delete, name="threads_delete"),
     # Posts
     re_path(r"^posts/$", posts.index, name="posts"),
     path("posts/<int:pk>/", posts.show, name="posts_show"),
-    re_path(r"^posts/new$", posts.new, name="posts_new"),
+    re_path(r"^posts/new/$", posts.new, name="posts_new"),
     path("posts/delete/<int:pk>/", posts.delete, name="posts_delete"),
     # Add Django site admin urls
     path("admin/", admin.site.urls),
